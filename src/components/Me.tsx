318de1f906a40b448bb987d326fccc91220e2e3a
mingli202 <vincentmingli@gmail.com>
2025-07-01 20:07:25 -0400

    added icons

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -1,38 +1,59 @@
-import { Envelope, Github, Linkedin } from "../lib/icons";
+import {
+  Angular,
+  Envelope,
+  Github,
+  Linkedin,
+  MapMarker,
+  NextJs,
+  TailwindCss,
+  TypeScript,
+} from "../lib/icons";
 
 export default function Me() {
   return (
-    <section className="flex justify-between">
-      <div className="flex flex-col justify-center gap-2">
-        <h1 className="text-5xl font-bold tracking-tight">Ming Li</h1>
+    <section className="flex justify-between gap-4">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-5xl font-bold tracking-tight">Ming Li Liu</h1>
         <p className="text-text-secondary">
           Average Rust enthusiast and Neovim enjoyer.
         </p>
+        <div className="flex items-center gap-2">
+          <TypeScript className="fill-text-secondary" />
+          <NextJs className="fill-text-secondary" />
+          <Angular className="fill-text-secondary" />
+          <TailwindCss className="fill-text-secondary" />
+        </div>
       </div>
-      <div className="flex flex-col items-end justify-center gap-1">
-        <div className="flex items-center gap-2">Montreal, Canada</div>
-        <div className="flex items-center gap-2">(514) 581-1268</div>
+      <div className="text-text-secondary flex flex-col items-end gap-1">
+        <div className="flex items-center gap-1">
+          <MapMarker className="fill-text-secondary" />
+          Montreal, Canada
+        </div>
+        <p>(514) 581-1268</p>
         <div className="flex items-center gap-2">
           <a
             href="https://github.com/mingli202"
             target="_blank"
             rel="noreferrer"
           >
-            <Github />
+            <Github className="fill-text-secondary" />
           </a>
           <a
-            href="www.linkedin.com/in/ming-li-liu"
+            href="https://www.linkedin.com/in/ming-li-liu"
             target="_blank"
             rel="noreferrer"
           >
-            <Linkedin />
+            <Linkedin className="fill-text-secondary" />
           </a>
           <a
             href="mailto:vincentmingli@gmail.com"
             target="_blank"
             rel="noreferrer"
           >
-            <Envelope />
+            <Envelope
+              className="fill-text-secondary"
+              title="vincentmingli@gmail.com"
+            />
           </a>
         </div>
       </div>
