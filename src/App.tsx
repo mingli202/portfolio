5255a235ff881f1f3f2ebe9ecc9097cf4b4ec3bf
mingli202 <vincentmingli@gmail.com>
2025-07-04 22:13:09 -0400

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
+import ExperienceList, {
+  type ExperienceListItem,
+} from "./components/ExperienceList";
 import Me from "./components/Me";
 import SectionBreakLine from "./components/SectionBreakLine";
 
 function App() {
+  const workExperience: ExperienceListItem[] = [
+    {
+      title: "C#/Web Developer Intern",
+      subtitle: "Genetec Inc.",
+      description:
+        "Genetec is a leading provider of software solutions for the biotech industry. I worked as a C#/Web Developer Intern at Genetec, where I was responsible for developing and maintaining the company's website and backend services.",
+      startDate: "May 2025",
+      endDate: "Aug 2025",
+      location: "Montreal, Canada",
+      skills: [
+        "TypeScript",
+        "Angular",
+        "Scss",
+        "C#",
+        "ASP.NET",
+        "SQL Server",
+        "Azure",
+      ],
+      additionalInfo: [
+        "I was responsible for developing and maintaining the company's website and backend services.",
+        "I worked on various projects, including the development of a new website design and the implementation of new features for the existing website.",
+        "I also assisted with the maintenance of the company's backend services, including the development of new APIs and the optimization of existing code.",
+        "Throughout my internship, I gained valuable experience in web development, C#, and database management.",
+      ],
+      imageUrl: "genetec_logo.jpg",
+    },
+  ];
+
+  const schoolExperience: ExperienceListItem[] = [
+    {
+      title: "B. Eng, Software Engineering Co-op",
+      subtitle: "McGill University",
+      description: "GPA: 3.94/4.0",
+      startDate: "Aug 2024",
+      endDate: "May 2028",
+      location: "Montreal, Canada",
+      skills: ["Java", "Python", "OOP"],
+      additionalInfo: [
+        "I studied computer science at McGill University and gained a strong foundation in programming and computer science concepts.",
+        "I was able to apply my knowledge of programming and computer science to various projects, including the development of a new website design and the implementation of new features for the existing website.",
+        "I also gained practical experience in software development, C#, and database management.",
+      ],
+      imageUrl: "mcgill_logo.jpg",
+    },
+  ];
+
   return (
-    <div className="flex w-[min(85%,45rem)] flex-col overflow-hidden py-16 text-xl">
+    <div className="flex w-[min(85%,45rem)] flex-col overflow-hidden text-xl">
       <Me />
-      <SectionBreakLine sectionName="Experience" />
+      <SectionBreakLine sectionName="Work" />
+      <ExperienceList items={workExperience} />
       <SectionBreakLine sectionName="Education" />
+      <ExperienceList items={schoolExperience} />
       <SectionBreakLine sectionName="Projects" />
       <SectionBreakLine sectionName="Contact" />
     </div>
