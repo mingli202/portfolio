48996eaaa9da21d94c56c177fad7ff48ba683df4
mingli202 <vincentmingli@gmail.com>
2025-07-04 22:13:05 -0400

    some styling

--- src/components/SectionBreakLine.tsx
+++ src/components/SectionBreakLine.tsx
@@ -4,9 +4,9 @@ type Props = {
 
 export default function SectionBreakLine({ sectionName }: Props) {
   return (
-    <div className="flex items-center gap-4 py-4 text-base">
+    <div className="flex items-center gap-4 py-4">
       <div className="bg-primary h-0.5 w-full rounded-full" />
-      <h2 className="text-primary">{sectionName}</h2>
+      <h2 className="text-primary font-thin">{sectionName}</h2>
       <div className="bg-primary h-0.5 w-full rounded-full" />
     </div>
   );
