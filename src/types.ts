a3c48fd87b897b6fab10ae7697c0edc7e82a5ba3
mingli202 <vincentmingli@gmail.com>
2025-07-04 22:20:32 -0400

    added types

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type ProjectItem = {
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  skills: string[];
+  additionalInfo: string[];
+};
+
+export type ExperienceListItem = {
+  title: string;
+  subtitle: string;
+  imageUrl: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  skills: string[];
+  additionalInfo: string[];
+};
