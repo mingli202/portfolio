754e264525c5f00badf1b299c42100a40c69d392
mingli202 <vincentmingli@gmail.com>
2025-09-19 18:18:35 -0400

    asdf

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -8,33 +8,44 @@ type Props = {
 export default function ExperienceList({ items, className, ...props }: Props) {
   return (
     <div className={cn("flex flex-col gap-2 p-2 md:p-4", className)} {...props}>
-      {items.map(({ title, imageUrl, subtitle, startDate, endDate }) => (
-        <div
-          key={title}
-          className={cn(
-            "border-secondary flex items-center gap-4 rounded-[calc(0.25rem+0.75rem)] border border-solid p-3 transition md:rounded-[calc(0.25rem+1rem)] md:p-4",
-
-            "hover:border-primary hover:ring-primary shadow-primary hover:bg-secondary/30 hover:cursor-pointer hover:shadow-[0_0_5px_2px_var(--primary-color),0_0_5px_2px_var(--primary-color)_inset] hover:ring-1",
-          )}
-        >
-          <img
-            src={imageUrl}
-            alt={title}
-            className="h-13 w-13 rounded md:h-16 md:w-16"
-            width={64}
-            height={64}
-          />
-          <div className="flex w-full flex-col gap-1">
-            <div className="flex w-full flex-wrap justify-between gap-x-4">
-              <p className="font-bold">{subtitle}</p>
-              <p className="text-text-secondary">
-                {startDate} {endDate ? `- ${endDate}` : null}
-              </p>
-            </div>
-            <p>{title}</p>
-          </div>
-        </div>
+      {items.map((item) => (
+        <ListItem key={item.title} {...item} />
       ))}
+    </div>
+  );
+}
+
+function ListItem({
+  title,
+  imageUrl,
+  subtitle,
+  startDate,
+  endDate,
+}: ExperienceListItem) {
+  return (
+    <div
+      className={cn(
+        "ring-secondary flex items-center gap-4 rounded-[calc(0.25rem+0.75rem)] p-3 ring-1 transition md:rounded-[calc(0.25rem+1rem)] md:p-4",
+
+        "hover:ring-primary hover:bg-secondary/30 hover:cursor-pointer hover:shadow-[0_0_10px_2px_var(--primary-color)] hover:ring-2",
+      )}
+    >
+      <img
+        src={imageUrl}
+        alt={title}
+        className="h-13 w-13 rounded md:h-16 md:w-16"
+        width={64}
+        height={64}
+      />
+      <div className="flex w-full flex-col gap-1">
+        <div className="flex w-full flex-wrap justify-between gap-x-4">
+          <p className="font-bold">{subtitle}</p>
+          <p className="text-text-secondary">
+            {startDate} {endDate ? `- ${endDate}` : null}
+          </p>
+        </div>
+        <p>{title}</p>
+      </div>
     </div>
   );
 }
