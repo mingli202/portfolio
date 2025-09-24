2d9d0fde7f7be4affeb0a84087a21a6923a13994
mingli202 <vincentmingli@gmail.com>
2025-09-24 19:21:44 -0400

    refac: move data to data folder

---
+++ src/data/schoolExperience.ts
@@ -0,0 +1,19 @@
+import type { ExperienceListItem } from "../types";
+
+export const schoolExperience: ExperienceListItem[] = [
+  {
+    title: "B. Eng, Software Engineering Co-op",
+    subtitle: "McGill University",
+    description: "GPA: 3.94/4.0",
+    startDate: "Aug 2024",
+    endDate: "May 2028",
+    location: "Montreal, Canada",
+    skills: [],
+    additionalInfo: [
+      "I studied computer science at McGill University and gained a strong foundation in programming and computer science concepts.",
+      "I was able to apply my knowledge of programming and computer science to various projects, including the development of a new website design and the implementation of new features for the existing website.",
+      "I also gained practical experience in software development, C#, and database management.",
+    ],
+    imageUrl: "mcgill_logo.jpg",
+  },
+];
---
+++ src/data/workExperience.ts
@@ -0,0 +1,21 @@
+import type { ExperienceListItem } from "../types";
+
+export const workExperience: ExperienceListItem[] = [
+  {
+    title: "C#/Web Developer Intern",
+    subtitle: "Genetec Inc.",
+    description:
+      "Genetec is a leading provider of software solutions for the biotech industry. I worked as a C#/Web Developer Intern at Genetec, where I was responsible for developing and maintaining the company's website and backend services.",
+    startDate: "May 2025",
+    endDate: "Aug 2025",
+    location: "Montreal, Canada",
+    skills: [],
+    additionalInfo: [
+      "I was responsible for developing and maintaining the company's website and backend services.",
+      "I worked on various projects, including the development of a new website design and the implementation of new features for the existing website.",
+      "I also assisted with the maintenance of the company's backend services, including the development of new APIs and the optimization of existing code.",
+      "Throughout my internship, I gained valuable experience in web development, C#, and database management.",
+    ],
+    imageUrl: "genetec_logo.jpg",
+  },
+];
