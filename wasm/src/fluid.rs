2815492cf316b9349ab9902f1d2e05153b430d3d
mingli202 <vincentmingli@gmail.com>
2025-10-29 21:54:46 -0400

    initial u, v, b, s

--- wasm/src/fluid.rs
+++ wasm/src/fluid.rs
@@ -1,8 +1,11 @@
 pub struct Fluid {
-    pub u: Vec<Vec<f32>>, // velocity in x direction
-    pub v: Vec<Vec<f32>>, // velocity in y direction
-    pub b: Vec<Vec<f32>>, // obstacles
-    pub s: Vec<Vec<f32>>, // smoke (density)
+    pub u: Vec<Vec<f32>>,      // velocity in x direction
+    pub v: Vec<Vec<f32>>,      // velocity in y direction
+    pub b: Vec<Vec<u8>>,       // obstacles
+    pub s: Vec<Vec<f32>>,      // smoke (density)
+    pub next_u: Vec<Vec<f32>>, // velocity in x direction
+    pub next_v: Vec<Vec<f32>>, // velocity in y direction
+    pub next_s: Vec<Vec<f32>>, // smoke (density)
 
     pub square_size: f32,
     pub grid_width: usize,
@@ -36,11 +39,23 @@ impl Fluid {
         let delta_t = delta_t.unwrap_or(1_f32 / 30_f32);
         let overrelaxation_coefficient = overrelaxation_coefficient.unwrap_or(1.7);
 
+        let u = vec![vec![0.0; min_squares]; min_squares + 1];
+        let v = vec![vec![0.0; min_squares + 1]; min_squares];
+        let b = vec![vec![1; min_squares + 1]; min_squares + 1];
+        let s = vec![vec![0.0; min_squares + 1]; min_squares + 1];
+
+        let next_u = vec![vec![0.0; u[0].len()]; u.len()];
+        let next_v = vec![vec![0.0; v[0].len()]; v.len()];
+        let next_s = vec![vec![0.0; s[0].len()]; s.len()];
+
         Fluid {
-            u: vec![vec![0.0; min_squares]; min_squares],
-            v: vec![vec![0.0; min_squares]; min_squares],
-            b: vec![vec![0.0; min_squares]; min_squares],
-            s: vec![vec![0.0; min_squares]; min_squares],
+            u,
+            v,
+            b,
+            s,
+            next_u,
+            next_v,
+            next_s,
 
             square_size,
             grid_width,
