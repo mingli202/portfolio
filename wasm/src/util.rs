a03adcf62e41c60878e6b52eacc7aa9774321368
mingli202 <vincentmingli@gmail.com>
2025-11-01 21:15:08 -0400

    fix gaussian

--- wasm/src/util.rs
+++ wasm/src/util.rs
@@ -1,6 +1,5 @@
 pub fn gaussian(x: i32, y: i32, sigma: f64) -> f64 {
     (-(x.pow(2) + y.pow(2)) as f64 / (2.0 * sigma * sigma)).exp()
-        / (2.0 * std::f64::consts::PI * sigma * sigma)
 }
 
 pub fn map(x: f64, in_min: f64, in_max: f64, out_min: f64, out_max: f64) -> f64 {
