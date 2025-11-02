901d933348b16cde19ffb3c150885d3250568e0e
mingli202 <vincentmingli@gmail.com>
2025-11-01 22:40:07 -0400

    added webassembly stuff?

---
+++ wasm/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const play: () => void;
+export const next_frame: () => void;
+export const toggle_playing: () => void;
+export const stop: () => void;
+export const print_fluid_info: () => void;
+export const run_projection: () => void;
+export const run_advection: () => void;
+export const clear_scene: () => void;
+export const run_solve_divergence_for_all: () => void;
+export const wasm_bindgen__convert__closures_____invoke__h6e056a96d2455b9d: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__habd4d36154439b3b: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hc09228ded1ded076: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
