c0b11019c8695f23894da0a72a57dce89319ea1a
mingli202 <vincentmingli@gmail.com>
2025-11-03 22:17:37 -0500

    drop resize callback

--- wasm/src/scene.rs
+++ wasm/src/scene.rs
@@ -579,13 +579,16 @@ impl Scene {
                 .cancel_animation_frame(id)
                 .expect("cancel_animation_frame error");
         }
-        s.animation_cb.take();
-        s.mouse_move_cb.take();
-        s.mouse_down_cb.take();
+
+        drop(s.animation_cb.take());
+        drop(s.mouse_move_cb.take());
+        drop(s.mouse_down_cb.take());
+        drop(s.resize_cb.take());
 
         window.set_onpointermove(None);
         window.set_onpointerdown(None);
         window.set_onpointerup(None);
+        window.set_onresize(None);
     }
 
     pub fn toggle_playing(self_ref: Rc<RefCell<Option<Self>>>) {
