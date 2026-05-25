215401492844f1b1b253136afa91c13c51c42bbe
mingli202 <vincentmingli@gmail.com>
2026-05-24 20:06:46 -0400

    added cargo icon

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,6 +14,10 @@ export const projects: ProjectItem[] = [
         type: Icon.Github,
         url: "https://github.com/mingli202/typing-test-tui",
       },
+      {
+        type: Icon.Cargo,
+        url: "https://crates.io/crates/typing-test-tui",
+      },
     ],
     videoUrl: "https://www.youtube.com/embed/Thjo68Ljbko?si=ia7GMkSZqPULdRfb",
   },
